71f8ce8695cf49371e2434c5252a6e36bf5e0ab4
Gabriel Beltrame Silva <beltrame@MacBook-Pro-de-Gabriel.local>
2020-04-06 23:41:29 -0300

    Listando livros

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "./styles.css";
+import Main from "./pages/main";
 
 import Header from "./components/Header";
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
 export default App;
-
-// Base URL
-
-// https://rocketseat-node.herokuapp.com/api
